293c1d0035be8e4b81e3d80f065af2d2add938d3
jacobfeldman96 <70081615+jacobfeldman96@users.noreply.github.com>
2021-03-01 11:45:54 -0500

    Second Push

--- Contracts/DeferredEquitySplitter.sol
+++ Contracts/DeferredEquitySplitter.sol
@@ -6,14 +6,10 @@ contract DeferredEquityPlan {
     address human_resources;
     address payable employee; // bob
     bool active = true; // this employee is active at the start of the contract
-
-    // @TODO: Set the total shares and annual distribution
     uint total_shares = 1000;
     uint annual_distribution = 250;
     
     uint start_time = now; // permanently store the time this contract was initialized
-
-    // @TODO: Set the `unlock_time` to be 365 days from now
     uint unlock_time = now + 365 days;
 
     uint public distributed_shares; // starts at 0
@@ -26,19 +22,10 @@ contract DeferredEquityPlan {
     function distribute() public {
         require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
         require(active == true, "Contract not active.");
-
-        // @TODO: Add "require" statements to enforce that:
-        // 1: `unlock_time` is less than or equal to `now`
-        // 2: `distributed_shares` is less than the `total_shares`
         require(unlock_time <= now);
         require(distributed_shares < total_shares);
-
-        // @TODO: Add 365 days to the `unlock_time`
         unlock_time + 365 days;
 
-        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
-        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
-        // Your code here!
         distributed_shares = ((now - start_time) / 365 days) * annual_distribution;
 
 
